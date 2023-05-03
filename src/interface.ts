d4a8578228ea8a83a8aceeca98c24373e967da11
anikahammedkhan <anikkhan1105@gmail.com>
2023-05-04 02:30:02 +0600

    interface added

--- src/interface.ts
+++ src/interface.ts
@@ -3,6 +3,11 @@ type User = {
     age: number;
 }
 
+type extendsUser = User & {
+    address: string;
+}
+
+
 interface IUser {
     name: string;
     age: number;
@@ -28,3 +33,29 @@ const userWithInterfaceExtends: extendsIUser = {
     age: 25,
     address: "Dhaka"
 }
+
+const userWithTypeAliasExtends: extendsUser = {
+    name: "Emni",
+    age: 25,
+    address: "Dhaka"
+}
+
+type rollNumber = number;
+
+type addNumberType = (num1: number, num2: number) => number;
+const addNumber: addNumberType = (num1, num2): number => num1 + num2;
+
+interface IAddNumbers {
+    (num1: number, num2: number): number;
+}
+
+const addNumbers: IAddNumbers = (num1, num2): number => num1 + num2;
+
+type rollNumbersType = number[];
+const rollNumbers: rollNumbersType = [1, 2, 3, 4, 5];
+
+interface IRollNumbers {
+    [index: number]: number;
+}
+
+const rollNumbersInterface: IRollNumbers = [1, 2, 3, 4, 5];
